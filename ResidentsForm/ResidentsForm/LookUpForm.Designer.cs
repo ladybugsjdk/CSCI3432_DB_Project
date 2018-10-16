2b9bac17dddd49d371670506b08ad1c575600c37
Jenna Lovett <jennablair12@gmail.com>
2018-10-16 14:40:48 -0400

    Add files via upload

---
+++ ResidentsForm/ResidentsForm/LookUpForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace ResidentsForm
+{
+    partial class LookUpForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LookUpForm));
+            this.lookupResident = new System.Windows.Forms.Label();
+            this.dgvResidentList = new System.Windows.Forms.DataGridView();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResidentList)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lookupResident
+            // 
+            this.lookupResident.AutoSize = true;
+            this.lookupResident.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lookupResident.Location = new System.Drawing.Point(318, 23);
+            this.lookupResident.Name = "lookupResident";
+            this.lookupResident.Size = new System.Drawing.Size(154, 24);
+            this.lookupResident.TabIndex = 0;
+            this.lookupResident.Text = "LookUp Resident";
+            // 
+            // dgvResidentList
+            // 
+            this.dgvResidentList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResidentList.Location = new System.Drawing.Point(53, 79);
+            this.dgvResidentList.Name = "dgvResidentList";
+            this.dgvResidentList.Size = new System.Drawing.Size(537, 340);
+            this.dgvResidentList.TabIndex = 1;
+            this.dgvResidentList.RowHeaderMouseClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dgvResidentList_RowHeaderMouseClick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(721, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(57, 47);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(647, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Send to Form";
+            this.button1.UseVisualStyleBackColor = true;
+           
+            // 
+            // LookUpForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.dgvResidentList);
+            this.Controls.Add(this.lookupResident);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "LookUpForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LookUpForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResidentList)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lookupResident;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        public System.Windows.Forms.DataGridView dgvResidentList;
+        private System.Windows.Forms.Button button1;
+    }
+}
